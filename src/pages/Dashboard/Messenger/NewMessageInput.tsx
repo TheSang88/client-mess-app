4b9e9c379040552777811cbafa16774aaa9fcbf1
TheSang88 <phamthesang88@gmail.com>
2023-10-20 12:28:52 +0700

    clientmes

---
+++ src/pages/Dashboard/Messenger/NewMessageInput.tsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import { styled } from "@mui/system";
+import { useAppSelector } from "../../../store";
+import { notifyTyping, sendDirectMessage, sendGroupMessage } from "../../../socket/socketConnection";
+import SendIcon from '@mui/icons-material/Send';
+const MainContainer = styled("div")({
+    height: "60px",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "10px",
+});
+
+const Input = styled("input")({
+    backgroundColor: "#2f3136",
+    width: "98%",
+    height: "44px",
+    color: "white",
+    border: "none",
+    borderRadius: "8px",
+    fontSize: "14px",
+    padding: "0 10px",
+    marginRight: "10px",
+});
+const Btn = styled("button")({
+    backgroundColor: "#2f3136",
+    width: "5%",
+    height: "44px",
+    color: "white",
+    border: "none",
+    borderRadius: "8px",
+    fontSize: "14px",
+    padding: "0 10px",
+    marginRight: "10px",
+});
+
+
+
+const NewMessageInput: React.FC = () => {
+    const [message, setMessage] = useState("");
+    const [focused, setFocused] = useState(false);
+
+    const onFocus = () => setFocused(true);
+    const onBlur = () => setFocused(false);
+
+    const { chosenChatDetails, chosenGroupChatDetails } = useAppSelector((state) => state.chat);
+
+
+
+    const handleSendMessage = (e: React.KeyboardEvent<HTMLInputElement>) => {
+
+        if (e.key === "Enter") {
+
+            if (chosenChatDetails) {
+                sendDirectMessage({
+                    message,
+                    receiverUserId: chosenChatDetails.userId!,
+                });
+            }
+
+            if (chosenGroupChatDetails) {
+                sendGroupMessage({
+                    message,
+                    groupChatId: chosenGroupChatDetails.groupId
+                })
+            }
+
+
+            setMessage("");
+        }
+    };
+    const btnmes = () => {
+
+        if (chosenChatDetails) {
+            sendDirectMessage({
+                message,
+                receiverUserId: chosenChatDetails.userId!,
+            });
+        }
+
+        if (chosenGroupChatDetails) {
+            sendGroupMessage({
+                message,
+                groupChatId: chosenGroupChatDetails.groupId
+            })
+        }
+
+
+        setMessage("");
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setMessage(e.target.value)
+    };
+
+
+    useEffect(() => {
+        // notify the receiverUser that the user(sender) is typing
+        if (chosenChatDetails?.userId) {
+            notifyTyping({
+                receiverUserId: chosenChatDetails.userId!,
+                typing: focused,
+            });
+        }
+    }, [focused, chosenChatDetails?.userId]);
+
+    return (
+        <MainContainer>
+            <Input
+                placeholder={chosenChatDetails ? `Write message to ${chosenChatDetails.username}` : "Your message..."}
+                value={message}
+                onChange={handleChange}
+                onKeyDown={handleSendMessage}
+                onFocus={onFocus}
+                onBlur={onBlur}
+            />
+            <Btn
+                onClick={btnmes}
+            >
+                <SendIcon />
+            </Btn>
+
+        </MainContainer>
+    );
+};
+
+export default NewMessageInput;
